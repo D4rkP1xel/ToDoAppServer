4643a46676ce0599e43dc6916a09e5170cd7fef9
Alex <alexcruzsilva12@gmail.com>
2023-02-01 22:50:02 +0000

    get route for tasks

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -3,6 +3,32 @@ import {v1 as uuidv1} from "uuid"
 import mysql from 'mysql2/promise'
 const router = Router()
 
+router.post("/get",  async(req:Request, res:Response)=>{
+    if(req.body == null || req.body.user_id == null || req.body.date == null)
+    {
+        res.status(403).json({error: "Bad Request Body"})
+        return
+    }
+    const query = `
+        SELECT *
+        FROM TASK
+        WHERE user_id='${req.body.user_id}' AND DATEDIFF('${req.body.date}', TASK.date)=0
+        `
+        try
+        {
+            const connection = await mysql.createConnection(process.env.DATABASE_URL || '')
+            const data = await connection.query(query)
+            res.status(200).json({message: "Task(s) retrived with success", tasks: data[0]})
+            return
+        }
+        catch(err)
+        {
+            console.log(err)
+            res.status(503).json({error: "Server Error"})
+            return
+        }
+})
+
 router.post("/add", async(req:Request, res:Response)=>{  // ADD TASK
     if(req.body == null || req.body.task_name == null || req.body.task_date == null || req.body.user_id == null)
     {
@@ -17,7 +43,7 @@ router.post("/add", async(req:Request, res:Response)=>{  // ADD TASK
         {
             const connection = await mysql.createConnection(process.env.DATABASE_URL || '')
             await connection.query(query)
-            res.status(200).json({messsage: "Task added with success"})
+            res.status(200).json({message: "Task added with success"})
             return
         }
         
@@ -45,7 +71,7 @@ router.post("/add", async(req:Request, res:Response)=>{  // ADD TASK
         {
             const connection = await mysql.createConnection(process.env.DATABASE_URL || '')
             await connection.query(query)
-            res.status(200).json({messsage: "Task removed with success"})
+            res.status(200).json({message: "Task removed with success"})
             return
         }
         
@@ -73,7 +99,7 @@ router.post("/add", async(req:Request, res:Response)=>{  // ADD TASK
         {
             const connection = await mysql.createConnection(process.env.DATABASE_URL || '')
             await connection.query(query)
-            res.status(200).json({messsage: "Task Name updated with success"})
+            res.status(200).json({message: "Task Name updated with success"})
             return
         }
         
@@ -101,7 +127,7 @@ router.post("/add", async(req:Request, res:Response)=>{  // ADD TASK
         {
             const connection = await mysql.createConnection(process.env.DATABASE_URL || '')
             await connection.query(query)
-            res.status(200).json({messsage: "Task Done Status updated with success"})
+            res.status(200).json({message: "Task Done Status updated with success"})
             return
         }
         
